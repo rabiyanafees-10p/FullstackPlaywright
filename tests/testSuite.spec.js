2eeca96fbd37d6246b922e58c8d64502f70f5e63
Rabiya Nafees <99785185+rabiyanafees-10p@users.noreply.github.com>
2024-03-19 23:50:13 +0500

    Update testSuite.spec.js
    updated text

--- tests/testSuite.spec.js
+++ tests/testSuite.spec.js
@@ -14,7 +14,7 @@ test.describe('My test suite', () => {
 });
 
 
-  test.only('Sync. Case 2', async ({ page }) => {
+  test('Sync. Case 2', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await expect(page).toHaveTitle('OrangeHRM');
     await page.click('input[name="username"]');
